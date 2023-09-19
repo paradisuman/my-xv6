f93e273952adfe1712fc3b84338cb729befdd4ac
Paradisum <2513209473@qq.com>
2023-09-19 19:57:05 +0800

    finish lab7

--- kernel/e1000.c
+++ kernel/e1000.c
@@ -40,6 +40,7 @@ e1000_init(uint32 *xregs)
   __sync_synchronize();
 
   // [E1000 14.5] Transmit initialization
+  // TDBAL: transmit descriptor base address （Low）
   memset(tx_ring, 0, sizeof(tx_ring));
   for (i = 0; i < TX_RING_SIZE; i++) {
     tx_ring[i].status = E1000_TXD_STAT_DD;
@@ -98,6 +99,47 @@ e1000_transmit(struct mbuf *m)
   //
   // Your code here.
   //
+  // 首先，通过读取 E1000_TDT 控制寄存器来询问 E1000 期望接收下一个数据包的 TX 环索引。
+  // printf("this is 001!\n");
+  acquire(&e1000_lock);
+  // regs[E1000_IMS] = 0;
+  // __sync_synchronize();
+
+  uint32 tdt = regs[E1000_TDT];
+  // 然后检查环是否已溢出。如果在由 E1000_TDT 索引的描述符中 E1000_TXD_STAT_DD 未设置，表示 E1000 尚未完成先前的传输请求，因此返回一个错误。
+  // printf("this is 002!\n");
+  if(tdt < 0 || tdt >= TX_RING_SIZE){
+    printf("tdt not valid! ");
+    release(&e1000_lock);
+    return -1;
+  }
+  if (!(tx_ring[tdt].status & E1000_TXD_STAT_DD)) {
+    release(&e1000_lock);
+    return -1;
+  }
+
+  // 否则，使用 mbuffree() 释放上一个从该描述符传输的 mbuf（如果有的话）。
+  // printf("this is 003!\n");
+  if (tx_mbufs[tdt]) {
+    // printf("this is %p!\n", tx_mbufs[tdt]);
+    mbuffree(tx_mbufs[tdt]);
+  }
+  
+  // 然后填充描述符。m->head 指向内存中数据包的内容，m->len 是数据包的长度。设置必要的命令标志（查看 E1000 手册的第 3.3 节）并存储 mbuf 的指针以便稍后释放。
+  tx_mbufs[tdt] = m;
+
+  tx_ring[tdt].addr = (uint64)tx_mbufs[tdt]->head;
+  tx_ring[tdt].length = (uint16)m->len;
+  tx_ring[tdt].status = 0;
+  tx_ring[tdt].cmd = E1000_TXD_CMD_EOP | E1000_TXD_CMD_RS;
+  // 最后，通过将 E1000_TDT 加 1（对 TX_RING_SIZE 取模）来更新环的位置。
+  // printf("this is 005!\n");
+  regs[E1000_TDT] = (regs[E1000_TDT] + 1) % TX_RING_SIZE;
+
+  // regs[E1000_IMS] = (1 << 7);
+  release(&e1000_lock);
+
+  // printf("this is tx!");
   // the mbuf contains an ethernet frame; program it into
   // the TX descriptor ring so that the e1000 sends it. Stash
   // a pointer so that it can be freed after sending.
@@ -112,6 +154,47 @@ e1000_recv(void)
   //
   // Your code here.
   //
+  while(1){
+    // 获取RDT寄存器的值，也就是下一个等待接收的数据包的环索引
+    uint32 rdt = regs[E1000_RDT];
+    
+    // 计算下一个索引（模以环大小）
+    uint32 next_rdt = (rdt + 1) % RX_RING_SIZE;
+    
+    // 检查是否有新的数据包可用
+    if (!(rx_ring[next_rdt].status & E1000_RXD_STAT_DD)) {
+      // release(&e1000_lock);
+      return;
+    }
+    
+    // 更新mbuf的长度
+    struct mbuf *m = rx_mbufs[next_rdt];
+    m->len = rx_ring[next_rdt].length;
+    
+    // 将mbuf传递给网络堆栈
+    net_rx(m);
+    
+    // 分配一个新的mbuf以替换旧的
+    struct mbuf *new_m = mbufalloc(0);
+    if (!new_m) {
+      // 处理分配失败
+      // release(&e1000_lock);
+      return;
+    }
+    
+    // 将新的mbuf数据指针编程到描述符中，并清除状态位
+    //rx_mbufs[next_rdt] = new_m;
+    rx_ring[next_rdt].addr = (uint64)new_m->head;
+    rx_ring[next_rdt].status = 0;
+    
+    // 保存新的mbuf指针
+    rx_mbufs[next_rdt] = new_m;
+    
+    // 更新RDT寄存器
+    regs[E1000_RDT] = next_rdt;
+  }
+
+  // printf("this is rx!");
   // Check for packets that have arrived from the e1000
   // Create and deliver an mbuf for each packet (using net_rx()).
   //
